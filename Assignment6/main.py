a5bd35f5a315733fe02efd8b397c8d8950b9fdca
christinafanwang <124625820+christinafanwang@users.noreply.github.com>
2023-04-01 16:04:16 -0700

    just need to add sequence

--- Assignment6/main.py
+++ Assignment6/main.py
@@ -1,91 +1,151 @@
 import js
 p5 = js.window
 
-state = 'PAUSE'
+program_state = 'start'
+#bg image
+bg = p5.loadImage('bg.png')  # load image data to bg
+#set font
+font = p5.loadFont('PressStart2P.otf')
 
 class Player:  
-    x = 0  # set x at 0
-    y = 0  # set y at 0
-    distance_x = 0
-    distance_y = 0
-    
-    def __init__(self, x, y, distance_x, distance_y):
-        self.x = x  # initialize attribute x 
-        self.y = y  # initialize attribute y 
-        self.distance_x = distance_x
-        self.distance_y = distance_y
+    x = 150  # set x at 150
+    y = 215  # set y at 225
 
-    def draw(self,r,g,b):
-        p5.fill(r,g,b)
-        p5.noStroke()
-        p5.push()
-        p5.translate(self.x, self.y)
-        p5.rect(-5, 5, 15, 5)
-        p5.rect(0, 0, 5, 15)
-        p5.pop()
-
-    def move_enemy(self):
-        self.x = self.x + self.distance_x
-        self.y = self.y + self.distance_y
+    player_img = p5.loadImage('player.png')  # load image data to player
     
+    def draw(self):
+        p5.image(self.player_img, self.x, self.y)
+
     def move_player(self, distance_x, distance_y):
         self.x = self.x + distance_x
         self.y = self.y + distance_y
-    
+
+class Iceberg:
+    x = 0
+    y = 0
+    speed = 0
+
+
+    def __init__(self, x, y, speed):
+        self.x = x
+        self.y = y
+        self.speed = speed
+        
+class Big(Iceberg):
+    ib_1 = p5.loadImage('ib_1.png')
+    def draw(self):
+        p5.image(self.ib_1, self.x, self.y)
+
+
+class Medium(Iceberg):
+    ib_2 = p5.loadImage('ib_2.png')
+    def draw(self):
+        p5.image(self.ib_2, self.x, self.y)
+
+class Small(Iceberg):
+    ib_3 = p5.loadImage('ib_3.png')
+    def draw(self):
+        p5.image(self.ib_3, self.x, self.y)
+
+class Background():
+    x = 0
+    y = 0
+    bg = p5.loadImage('bg.png')
+    def draw(self):
+        p5.image(self.bg, self.x, self.y)
+
 def setup():
     p5.createCanvas(300, 300) 
     print('finished setup') 
-player = Player(150,150,0,0)
-enemy = Player(100,100,p5.random(0,2),p5.random(4,6))
+
+player = Player()    
+ib_1 = Big(p5.random(1,300),0,0.75)
+ib_2 = Medium(p5.random(1,300),0,1.5)
+ib_3 = Small(p5.random(1,300),0,2)
+background = Background()
 
 def draw():
-    p5.background(255)        
-    player.draw(245, 221, 66)
-    enemy.draw(198, 29, 245)
-    enemy.move_enemy()
-
-    #setting limit for enemy
-    if(300 < enemy.x or 0 > enemy.x):
-        enemy.distance_x *= -1
-    if(300 < enemy.y or 0 > enemy.y):
-        enemy.distance_y *= -1
-    
-    dis = p5.dist(player.x , player.y , enemy.x , enemy.y)
-
-    if(dis <= 15):
-        player.distance_x = 0
-        player.distance_y = 0
-        enemy.distance_x = 0
-        enemy.distance_y = 0
-
-        p5.fill(0)
-        p5.textSize(27)
-        p5.text("TRY AGAIN",100,100)
+    global program_state
+    p5.background(255) 
+    background.draw()
+    if (program_state == 'start'):
+        p5.textFont(font)
+        p5.textSize(20)
+        p5.text('ARCTIC VOYAGE', 25, 115)
+        p5.textSize(10)
+        p5.text('click anywhere to start!', 40,130) 
         
-def keyPressed(event):
-    if(300 > player.x and player.x > 0):
-        if(p5.keyCode == p5.RIGHT_ARROW):
-           print('move point 10 pixels to the right..')
-           player.move_player(10, 0)
-        if(p5.keyCode == p5.LEFT_ARROW):
-            print('move point 10 pixels to the left..')
-            player.move_player(-10, 0)
-
-    if(0 >= player.x):
-        if(p5.keyCode == p5.RIGHT_ARROW):
-            print('move point 10 pixels to the right..')
-            player.move_player(10, 0) 
-
-    if(300 <= player.x):
-        if(p5.keyCode == p5.LEFT_ARROW):
-            print('move point 10 pixels to the left..')
-            player.move_player(-10, 0)  
+    player.draw()
+    
+    dis1 = p5.dist(player.x , player.y , ib_1.x , ib_1.y)
+    dis2 = p5.dist(player.x , player.y , ib_2.x , ib_2.y)
+    dis3 = p5.dist(player.x , player.y , ib_3.x , ib_3.y)
+    
+    if (program_state == 'play'):
+        p5.fill(255)
+        
+        ib_1.draw()
+        if (ib_1.y == 300):
+            resetProj(ib_1)
             
+        ib_2.draw()
+        if (ib_2.y == 300):
+            resetProj(ib_2)
+            
+        ib_3.draw()
+        if (ib_3.y == 300):
+            resetProj(ib_3)
+
+        ib_1.y = ib_1.y + ib_1.speed
+        ib_2.y = ib_2.y + ib_2.speed
+        ib_3.y = ib_3.y + ib_3.speed
+    
+    
+    if (dis1 <= 25 or dis2 <= 25 or dis3 <= 25):
+
+        program_state = 'gameover'
+        
+        p5.fill(0)
+        p5.textFont(font)
+        p5.textSize(20)
+        p5.text("TRY AGAIN",50,100)
+        p5.textSize(9)
+        p5.text("click anywhere to restart game",15,125)
+
+def resetProj(proj):
+    proj.y = 0
+    proj.x = p5.random(1,300)
+    
+def keyPressed(event): 
+    global program_state
+        
+    if(program_state == 'play'):
+        if(300 > player.x and player.x > 0):
+            if(p5.keyCode == p5.RIGHT_ARROW):
+                player.move_player(10, 0)
+            if(p5.keyCode == p5.LEFT_ARROW):
+                player.move_player(-10, 0)
+                
+        if(0 >= player.x):
+            if(p5.keyCode == p5.RIGHT_ARROW):
+                player.move_player(10, 0) 
+                
+        if(300 <= player.x):
+            if(p5.keyCode == p5.LEFT_ARROW):
+                player.move_player(-10, 0)   
+
 def keyReleased(event):
     pass
 
 def mousePressed(event):
-    pass
+    global program_state
+    
+    if(program_state == 'start'):
+        program_state = 'play'
+
+    elif(program_state == 'gameover'):
+        program_state = 'start'
 
 def mouseReleased(event):
     pass
+    
